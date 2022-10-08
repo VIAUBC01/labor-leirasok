f63e867a27598c9498d13c024ed31882d708a9ea
Simon Gábor <simon.gabor@vik.bme.hu>
2022-10-08 17:49:48 +0200

    web api snippets

---
+++ webapi/snippets/Dtos/TitleQueryModel.cs
@@ -0,0 +1,31 @@
+﻿using MovieCatalogApi.Entities;
+
+namespace MovieCatalogApi.Dtos;
+
+public class TitleQueryModel
+{
+    public TitleQueryModel(Title title, int[] genreIDs)
+    {
+        Id = title.Id;
+        PrimaryTitle = title.PrimaryTitle;
+        OriginalTitle = title.OriginalTitle;
+        TitleType = title.TitleType;
+        StartYear = title.StartYear;
+        EndYear = title.EndYear;
+        RuntimeMinutes = title.RuntimeMinutes;
+        //AverageRating = title.AverageRating;
+        //NumberOfVotes = title.NumberOfVotes;
+        Genres = genreIDs;
+    }
+
+    public int Id { get; set; }
+    public string PrimaryTitle { get; set; }
+    public string? OriginalTitle { get; set; }
+    public TitleType TitleType { get; set; }
+    public int? StartYear { get; set; }
+    public int? EndYear { get; set; }
+    public int? RuntimeMinutes { get; set; }
+    //public float? AverageRating { get; set; }
+    //public int NumberOfVotes { get; set; }
+    public IEnumerable<int>? Genres { get; set; }
+}
---
+++ webapi/snippets/Entities/MovieCatalogDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieCatalogApi.Entities;
+
+public class MovieCatalogDbContext : DbContext
+{
+    public MovieCatalogDbContext(ILogger<MovieCatalogDbContext> logger
+        , DbContextOptions<MovieCatalogDbContext> options) : base(options)
+    {
+        Logger = logger;
+    }
+    private ILogger<MovieCatalogDbContext> Logger { get; }
+
+    public DbSet<Title> Titles => Set<Title>();
+
+    public DbSet<Genre> Genres => Set<Genre>();
+
+    public DbSet<TitleGenre> TitleGenres => Set<TitleGenre>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Title>(title =>
+        {
+            title.Property(t => t.PrimaryTitle)
+                .HasMaxLength(500);
+            title.HasIndex(t => t.PrimaryTitle);
+            title.HasIndex(t => t.TitleType);
+        });
+
+        modelBuilder.Entity<TitleGenre>().HasIndex(tg => new { tg.GenreId, tg.TitleId }).IsUnique();
+
+        modelBuilder.Entity<Genre>(genre =>
+        {
+            genre.Property(g => g.Name).HasMaxLength(50);
+            genre.HasIndex(g => g.Name).IsUnique();
+        });
+    }
+}
