018a855f82ea084c62e5a8b7a88232d56bba5941
imre-gabor <imre-gabor@users.noreply.github.com>
2025-09-09 16:24:14 +0000

    deploy: aa88f9bd3cd06c59ecb834cb84db378070523f51

--- laborok/spring-cloud/bonus/src/main/java/hu/bme/aut/szoftlab/bonus/repository/BonusRepository.java
+++
@@ -1,8 +0,0 @@
-package hu.bme.aut.szoftlab.bonus.repository;
-
-import hu.bme.aut.szoftlab.bonus.model.Bonus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BonusRepository extends JpaRepository<Bonus, String>{
-
-}
