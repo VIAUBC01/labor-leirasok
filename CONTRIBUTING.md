e0c8e8d81dd9de61eb68064f82c6efbb14451294
Attila Hideg <hideg.attila@aut.bme.hu>
2025-10-15 08:19:06 +0200

    Snippets for web-api

--- CONTRIBUTING.md
+++
@@ -1,3 +0,0 @@
-# Hozzájárulás az anyaghoz
-
-A hozzájárulásról [itt](docs/tudnivalok/github/contributing.md) olvashatsz.
---
+++ Entities/TitleGenre.cs
@@ -0,0 +1,15 @@
+﻿namespace MovieCatalogApi.Entities;
+
+public class TitleGenre
+{
+    public int Id { get; set; }
+
+    public int TitleId { get; set; }
+
+    public Title Title { get; set; } = null!;
+
+    public int GenreId { get; set; }
+
+    public Genre Genre { get; set; } = null!;
+
+}
